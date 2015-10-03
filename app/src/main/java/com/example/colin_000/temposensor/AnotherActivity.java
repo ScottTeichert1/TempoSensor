6ca4f7ecb4affe92b86b120fe1093fda483e9feb
Scott <raddude1192@gmail.com>
2015-10-03 15:20:03 -0700

    Scott's new stuff

--- app/src/main/java/com/example/colin_000/temposensor/AnotherActivity.java
+++ app/src/main/java/com/example/colin_000/temposensor/AnotherActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class AnotherActivity extends AppCompatActivity {
@@ -15,5 +17,28 @@ public class AnotherActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(), "Here is out login, guys.", Toast.LENGTH_LONG).show();
 
+        Button b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AnotherActivity.this, "Clicked blue button!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button b2 = (Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AnotherActivity.this, "Clicked green button!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button b3 = (Button)findViewById(R.id.button3);
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AnotherActivity.this, "Clicked yellow button!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

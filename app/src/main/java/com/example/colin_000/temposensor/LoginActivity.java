fff1be36d167468b3444e378bd82325cefaa8f67
Colin Frerichs <colin.frerichs@gmail.com>
2015-10-03 22:22:48 -0700

    Changed textfields to edittexts so we can get the values out of them.

--- app/src/main/java/com/example/colin_000/temposensor/LoginActivity.java
+++ app/src/main/java/com/example/colin_000/temposensor/LoginActivity.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private TextView mUserName;
-    private TextView mPasswordField;
+    private EditText mUserName;
+    private EditText mPasswordField;
     private Button mLoginButton;
 
 
@@ -21,8 +21,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mLoginButton = (Button) findViewById(R.id.loginButton);
-        mUserName = (TextView) findViewById(R.id.userName);
-        mPasswordField = (TextView) findViewById(R.id.userPassword);
+        mUserName = (EditText) findViewById(R.id.userName);
+        mPasswordField = (EditText) findViewById(R.id.userPassword);
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,8 +30,8 @@ public class LoginActivity extends AppCompatActivity {
                 if (mUserName.getText().toString().equals("Login") && mPasswordField.getText().toString().equals("password")) {
                     startHomepage();
                 } else {
-                    Toast.makeText(LoginActivity.this, "Sorry, that password is incorrect", Toast.LENGTH_LONG);
                     stayOnPage();
+                    Toast.makeText(LoginActivity.this, "Sorry, that password is incorrect", Toast.LENGTH_LONG).show();
                 }
             }
         });
